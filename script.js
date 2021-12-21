c224cba9b8b7d1c479cf3a062b9bc9f6a0033ff1
Isaac Willian <isaaacwillian@gmail.com>
2021-12-21 15:38:04 -0300

    Adicionado uma linha no js para resetar o que escrever no input

--- script.js
+++ script.js
@@ -6,18 +6,20 @@ let array = [];
 let arrayData = JSON.parse(localStorage.getItem("data"));
 Array.prototype.push.apply(array, arrayData);
 for (element of array) {
-    div.innerHTML = div.innerHTML + "<h1></h1>";
+    div.innerHTML += "<h1></h1>";
     div.children[div.children.length - 1].innerHTML = element;
 }
 function task() {
-    div.innerHTML = div.innerHTML + "<h1></h1>";
+    div.innerHTML += "<h1></h1>";
     div.children[div.children.length - 1].innerHTML = input.value;
     array.push(input.value);
+    document.getElementById('input').value = ''
     localStorage.setItem("data", JSON.stringify(array));
 }
 function reset() {
     while (div.firstChild) {
         div.removeChild(div.firstChild);
     }
+    array = [];
     localStorage.clear();
 }
